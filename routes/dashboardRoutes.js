91546b3101ce9205fbb90540156463c1c9ce6892
Asghar-18 <Asgharali23890@gmail.com>
2024-01-01 11:03:39 +0500

    minor changes

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -6,10 +6,6 @@ router.get('/dashboard', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'views', 'admin', 'index.html'));
 });
 
-router.get('/profile', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'views', 'admin', 'profile.html'));
-});
-
 router.get('/table', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'views', 'admin', 'table.html'));
 });
